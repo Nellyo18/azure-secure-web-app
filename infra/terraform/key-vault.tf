8eca8a7c17e6446bda1c8191538390edc9005d09
Nellyo18 <nelsondouglas1848@gmail.com>
2026-09-09 16:10:06 -0700

    Add Azure Key Vault and related resources

---
+++ infra/terraform/key-vault.tf
@@ -0,0 +1,46 @@
+resource "azurerm_key_vault" "app" {
+  name                = "kv-secure-web-nelson"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tenant_id           = var.tenant_id
+
+  sku_name = "standard"
+
+  enable_rbac_authorization       = true
+  public_network_access_enabled   = false
+  purge_protection_enabled        = false
+  soft_delete_retention_days      = 90
+}
+
+resource "azurerm_private_dns_zone" "key_vault" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "key_vault" {
+  name                  = "link-kv-vnet"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.key_vault.name
+  virtual_network_id    = azurerm_virtual_network.main.id
+
+  registration_enabled = false
+}
+
+resource "azurerm_private_endpoint" "key_vault" {
+  name                = "pe-kv-secure-web-prod"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = azurerm_subnet.private_endpoints.id
+
+  private_service_connection {
+    name                           = "psc-kv-secure-web-prod"
+    private_connection_resource_id = azurerm_key_vault.app.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [azurerm_private_dns_zone.key_vault.id]
+  }
+}
