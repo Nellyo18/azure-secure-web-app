037326baa70e1b2b6933095d1bf506121c608f4c
Nellyo18 <nelsondouglas1848@gmail.com>
2026-09-09 17:53:03 -0700

    Rename RBAC authorization variable for consistency

--- infra/terraform/key-vault.tf
+++ infra/terraform/key-vault.tf
@@ -6,7 +6,7 @@ resource "azurerm_key_vault" "app" {
 
   sku_name = "standard"
 
-  enable_rbac_authorization       = true
+  rbac_authorization_enabled = true
   public_network_access_enabled   = false
   purge_protection_enabled        = false
   soft_delete_retention_days      = 90
