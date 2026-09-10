0227495c0f267ac026360324133c90f1e7bdd348
Nellyo18 <nelsondouglas1848@gmail.com>
2026-09-09 17:50:22 -0700

    Update private DNS zone link and endpoint configuration

--- infra/terraform/sql.tf
+++ infra/terraform/sql.tf
@@ -30,25 +30,23 @@ resource "azurerm_private_dns_zone" "sql" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "sql" {
-  name                  = "link-sql-vnet"
+  name                  = "mxqjzf6kava2w"
   resource_group_name   = azurerm_resource_group.main.name
   private_dns_zone_name = azurerm_private_dns_zone.sql.name
   virtual_network_id    = azurerm_virtual_network.main.id
 
   registration_enabled = false
 }
-
 resource "azurerm_private_endpoint" "sql" {
   name                = "pe-sql-secure-web-prod"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   subnet_id           = azurerm_subnet.private_endpoints.id
 
-  private_service_connection {
-    name                           = "pe-sql-secure-web-prod"
-    private_connection_resource_id = azurerm_mssql_server.sql.id
-    subresource_names              = ["sqlServer"]
-    is_manual_connection           = false
-  }
+ private_service_connection {
+  name                           = "pe-sql-secure-web-prod"
+  private_connection_resource_id = azurerm_mssql_server.sql.id
+  subresource_names              = ["sqlServer"]
+  is_manual_connection           = false
 }
  
